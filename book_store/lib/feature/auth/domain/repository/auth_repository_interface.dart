9c0e08a6f99850e835af87b74129f85c6ed878da
Mahmoud <mahmoudsayeedr80@gmail.com>
2026-03-23 09:37:32 +0200

    simplify architecture and remove useless usecases

--- book_store/lib/feature/auth/domain/repository/auth_repository_interface.dart
+++
@@ -1,6 +0,0 @@
-import '../../data/models/user_model.dart';
-
-abstract class AuthRepositoryInterface {
-    Future<UserModel>login(String email,String password);
-    Future<UserModel>register(UserModel user);
-}
